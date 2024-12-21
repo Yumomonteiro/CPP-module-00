0cca35a5641c1697a6736bde178cc2863fc57e47
yumomonteiro <yuriaco@hotmail.com>
2024-12-21 17:40:03 +0000

    finish ex02

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -12,7 +12,7 @@
 
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap() :: ClapTrap() {
+FragTrap::FragTrap() : ClapTrap() {
         std::cout << "FragTrap is born!" << std::endl;
         this->_name = "";
         this->_hitPoints = 100;
@@ -20,7 +20,11 @@ FragTrap::FragTrap() :: ClapTrap() {
         this->_attackDamage = 30;
 }
 
-FragTrap::FragTrap(std::string name) : _name(name), _hitPoints(100), _energyPoints(100), _attackDamage(30) :: ClapTrap(name) {
+FragTrap::FragTrap(std::string name) :ClapTrap(name) {
+        this->_name = "";
+        this->_hitPoints = 100;
+        this->_energyPoints = 50;
+        this->_attackDamage = 20;
         std::cout << "FragTrap " << _name << " is born!" << std::endl;
 }
 
@@ -28,7 +32,7 @@ FragTrap::~FragTrap(void) {
         std::cout << "FragTrap Destructor is called " << _name << std::endl;
 }
 
-FragTrap::FragTrap(const FragTrap &copy) :: ClapTrap(copy) {
+FragTrap::FragTrap(const FragTrap &copy) : ClapTrap(copy) {
         *this = copy;
 }
 
